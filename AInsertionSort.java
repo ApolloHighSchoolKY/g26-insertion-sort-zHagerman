14bea03b637c31d7214672d88fc92e734ac7cf70
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-04-21 03:06:12 +0000

    done

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
-
+        int temp2;
+        for(int i = 1; i < myNumbers.length;i++)
+		{
+			for(int j = i-1; j > 0; j--)
+			{
+				if(myNumbers[j] < myNumbers[j-1])
+				{
+					temp2 = myNumbers[j];
+					myNumbers[j] = myNumbers[j-1];
+					myNumbers[j-1] = temp2;
+				}
+				else
+				{
+					temp2 = myNumbers[j];
+				}
+				System.out.println(Arrays.toString(myNumbers));
+			}
+		}
+        System.out.println("my sorted array " + Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -8,22 +9,29 @@ public class ALInsertionSort
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
-
+		
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
-			//Check with each of the numbers in the sorted list
-
-				//If this number is less than one in the sorted list,
-				//insert it there
-
-			//If it was not inserted, stick it on the end.
-
-
-		//End Loop for unsorted list
-
-		System.out.println(sorted);
+		for(int i = 1; i < myNumbers.length;i++)
+		{
+			inserted = false;
+			for(int j = 0; j < sorted.size() && inserted == false; j++)
+			{
+				if(myNumbers[i] < sorted.get(j))
+				{
+					sorted.add(j,myNumbers[i]);
+					inserted = true;
+				}
+				
+				
+			}
+			if(inserted == false)
+			{
+				sorted.add(myNumbers[i]);
+			}
+		}
+        System.out.println("my sorted arraylist " + (sorted.toString()));
 
 		//Move the data back to the array
 
